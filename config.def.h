998f2a6835b2080c80f360130199742e5539a066
deyannis <deyannis@csd.uoc.gr>
2025-02-14 20:45:50 +0200

    feat: border patch, increase alpha, center vert

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@
 static int topbar = 1;                        /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                      /* -c option; centers dmenu on screen */
 static int min_width = 500;                   /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
-static const unsigned int alpha = 0xcc;       /* Amount of opacity. 0xff is opaque             */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
+static const unsigned int alpha = 0xee;       /* Amount of opacity. 0xff is opaque             */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -31,3 +31,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 1;
--- patchlist.md
+++ patchlist.md
@@ -3,3 +3,4 @@ List of patches
 
 * dmenu-alpha-20230110-5.2.diff
 * dmenu-center-20240616-36c3d68.diff
+* dmenu-border-20230512-0fe460d.diff
