a69fe1f616685405f26497946e5b7db6fe7b8c35
deyannis <deyannis@csd.uoc.gr>
2025-02-14 22:02:42 +0200

    ref: border style update

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm]   = { "#d8dee9", "#2e3440" },
 	[SchemeSel]    = { "#2e3440", "#a3be8c" },
 	[SchemeOut]    = { "#2e3440", "#8fbcbb" },
-	[SchemeBorder] = { "#4c566a", NULL },
+	[SchemeBorder] = { "#5e81ac", NULL },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
@@ -34,4 +34,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 1;
